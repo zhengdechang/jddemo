228acba85d404855be79b8f3488e44917d9013a7
zhengdechang <2473023641@qq.com>
2021-04-07 17:32:30 +0800

    '网络请求封装'

--- src/network/axios.js
+++ src/network/axios.js
@@ -19,20 +19,27 @@ function get(url,params={}){
 /**
  * 封装post方法
  */
-// function post(url,data={}){
-//     return new Promise((resolve,reject) => {
-//         axios.post(url,data)
-//         .then(response =>{
-//             resolve(response.data);
-//         })
-//         .catch(err =>{
-//             reject(err);
-//         })
-//     });
-// }
+function post(url,data={}){
+    return new Promise((resolve,reject) => {
+        axios.post(url,data)
+        .then(response =>{
+            resolve(response.data);
+        })
+        .catch(err =>{
+            reject(err);
+        })
+    });
+}
 
 
 export const Swiper =  () => get('http://localhost:3000/test/swiper',{});
 export const Otherapp =  () => get('http://localhost:3000/test/otherapp',{});
 export const Spikes  = ()  => get('http://localhost:3000/test/spike',{});
 export const Likes  = ()  => get('http://localhost:3000/test/like',{});
+
+
+//post
+export const postLikes  = ()  => post('http://localhost:3000/test/like',{});
+
+
+
